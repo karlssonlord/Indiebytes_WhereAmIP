1e790c64e52311fa0bbba27705be122e55a916c1
Robert Lord <robert@codepeak.se>
2013-09-06 17:15:34 +0200

    Function for fetching the current redirect URL

--- app/code/community/Indiebytes/WhereAmIP/Helper/Data.php
+++ app/code/community/Indiebytes/WhereAmIP/Helper/Data.php
@@ -3,6 +3,66 @@ class Indiebytes_WhereAmIP_Helper_Data extends Mage_Core_Helper_Abstract
 {
     private $_selectableCountries;
 
+    /**
+     * Get URL where we should redirect
+     *
+     * @author Robert Lord <robert@karlssonlord.com>
+     *
+     * @return string|boolean
+     */
+    public function getRedirectUrl()
+    {
+        /**
+         * Setup the default return result
+         */
+        $return = FALSE;
+
+        /**
+         * Fetch current store code
+         */
+        $storeCode = Mage::getSingleton('core/session')->getStoreCode();
+
+        /**
+         * Fetch current URL
+         */
+        $currentUrl = Mage::helper('core/url')->getCurrentUrl();
+
+        /**
+         * Fetch URL for store code
+         */
+        $storeUrl = Mage::getModel('core/store')->load($storeCode)->getUrl();
+
+        /**
+         * Make sure the URL's has trailing / when comparing
+         */
+        if (substr($storeUrl, -1) !== '/') {
+            $storeUrl .= '/';
+        }
+
+        if (substr($currentUrl, -1) !== '/') {
+            $currentUrl .= '/';
+        }
+
+        /**
+         * Compare the URLs
+         */
+        if (substr($currentUrl, 0, (strlen($storeUrl))) !== $storeUrl) {
+            /**
+             * Remove trailing slashes
+             */
+            if (substr($storeUrl, -1) == '/') {
+                $storeUrl = substr($storeUrl, 0, (strlen($storeUrl) - 1));
+            }
+
+            /**
+             * Set the redirect URL
+             */
+            $return = $storeUrl . $_SERVER['REQUEST_URI'];
+        }
+
+        return $return;
+    }
+
     public function getActiveCountries()
     {
         if ( $this->_selectableCountries ) {
