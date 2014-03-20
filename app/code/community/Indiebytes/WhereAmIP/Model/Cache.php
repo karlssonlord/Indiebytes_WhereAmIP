2daf28fe69a18d99fd73dd4cebc57779e0e8a712
Jacob Klapwijk <hello@jcbk.me>
2014-03-20 16:43:49 +0100

    WIP: full page cache

---
+++ app/code/community/Indiebytes/WhereAmIP/Model/Cache.php
@@ -0,0 +1,20 @@
+<?php
+class Indiebytes_WhereAmIP_Model_Cache extends Enterprise_PageCache_Model_Container_Abstract
+{
+  protected function _getCacheId()
+   {
+      return 'CUSTOMER_STATE_' . md5($this->_placeholder->getAttribute('cache_id') . $this->_getCookieValue(Enterprise_PageCache_Model_Cookie::COOKIE_CUSTOMER, ''));
+   }
+
+  protected function _renderBlock()
+   {
+       $block = $this->_placeholder->getAttribute('block');
+       $template = $this->_placeholder->getAttribute('template');
+
+       $block = new $block;
+       $block->setTemplate($template);
+       $block->setLayout(Mage::app()->getLayout());
+
+       return $block->toHtml();
+   }
+}
