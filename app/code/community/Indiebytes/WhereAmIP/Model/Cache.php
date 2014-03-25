f673da30c8b40ca35ead9f1092473336035dfdb6
Erik Eng <mail@ptz0n.se>
2014-03-25 12:25:17 +0100

    Hotfix: Invalidating country link each second.

--- app/code/community/Indiebytes/WhereAmIP/Model/Cache.php
+++ app/code/community/Indiebytes/WhereAmIP/Model/Cache.php
@@ -1,20 +1,22 @@
 <?php
 class Indiebytes_WhereAmIP_Model_Cache extends Enterprise_PageCache_Model_Container_Abstract
 {
-  protected function _getCacheId()
-   {
-      return 'CUSTOMER_STATE_' . md5($this->_placeholder->getAttribute('cache_id') . $this->_getCookieValue(Enterprise_PageCache_Model_Cookie::COOKIE_CUSTOMER, ''));
-   }
+    protected function _getCacheId()
+    {
+        return 'CONTAINER_' . md5(
+            $this->_placeholder->getAttribute('cache_id') .
+            $this->_getCookieValue(Enterprise_PageCache_Model_Cookie::COOKIE_CUSTOMER, ''));
+    }
 
-  protected function _renderBlock()
-   {
-       $block = $this->_placeholder->getAttribute('block');
-       $template = $this->_placeholder->getAttribute('template');
+    protected function _renderBlock()
+    {
+        $block = $this->_placeholder->getAttribute('block');
+        $template = $this->_placeholder->getAttribute('template');
 
-       $block = new $block;
-       $block->setTemplate($template);
-       $block->setLayout(Mage::app()->getLayout());
+        $block = new $block;
+        $block->setTemplate($template);
+        $block->setLayout(Mage::app()->getLayout());
 
-       return $block->toHtml();
-   }
+        return $block->toHtml();
+    }
 }
